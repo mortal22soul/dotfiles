7450fc0cc7d99f033e8092aae29743d0b05a2fa9
Mortal22Soul <99786026+Mortal22Soul@users.noreply.github.com>
2022-03-23 22:22:45 +0530

    Add files via upload

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -7,8 +7,14 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
-polybar main -r &
-polybar secondary -r &
-polybar bottom -r &
+
+if [[ $DESKTOP_SESSION == "i3" || $DESKTOP_SESSION == "i3-with-shmlog" ]]; then
+    polybar main -r &
+    polybar secondary -r &
+elif [[ $DESKTOP_SESSION == "bspwm" ]]; then
+    polybar main -r &
+    polybar secondary -r &
+    polybar bottom -r &
+fi
 
 echo "Bars launched..."
